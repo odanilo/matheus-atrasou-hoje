040b4df6f6e5e15babd7a296b3b90d66ae9588d0
Danilo <dan.nunes0@gmail.com>
2023-09-08 14:00:34 -0300

    feat: add custom ziclets font

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,19 @@ export default function App() {
   return (
     <html lang="en" className="h-full">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href="https://use.typekit.net/lzl4cqa.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        artistic: ["ziclets", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
