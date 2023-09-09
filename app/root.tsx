ef7d6ce81a3fb6679e5bdffa4a0e7f4ee5d2aae4
Danilo <dan.nunes0@gmail.com>
2023-09-09 01:38:38 -0300

    feat: add header styles (#1)
    Creates the styles for the homepage header and removes files that are not currently being used.

--- app/root.tsx
+++ app/root.tsx
@@ -26,12 +26,23 @@ export default function App() {
   return (
     <html lang="en" className="h-full">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href="https://use.typekit.net/lzl4cqa.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="h-full bg-zinc-950 text-zinc-50">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
