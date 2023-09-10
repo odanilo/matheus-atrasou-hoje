a26ac91925bd81721d6202641fa52c65ed2ed7dc
Danilo <dan.nunes0@gmail.com>
2023-09-10 20:33:07 -0300

    feat: vomits

--- app/components/delay-card.tsx
+++ app/components/delay-card.tsx
@@ -24,11 +24,11 @@ export function DelayCard({
       className="flex flex-col transition-transform will-change-transform hover:-translate-y-1"
       {...props}
     >
-      <article className="flex flex-1 flex-col">
+      <article className="flex flex-1 flex-col relative bg-zinc-900 rounded border border-dashed border-zinc-700">
         <Link
           to={`/atrasos/${delay.id}`}
           prefetch="intent"
-          className="flex flex-1 gap-4 p-6 bg-zinc-900 rounded border border-dashed border-zinc-700"
+          className="flex flex-1 gap-4 p-6"
         >
           <div className="flex shrink-0 rounded-full shadow items-center justify-center h-12 w-12 bg-amber-400">
             🤬
@@ -40,18 +40,27 @@ export function DelayCard({
             </header>
             <h2 className="text-amber-400">{delay.title}</h2>
             <div className="mt-2">{delay.body}</div>
-            <footer className="flex mt-auto pt-4 justify-end text-zinc-500">
-              <Form method="post" className={vomitActionClasses}>
-                <button type="submit" className="flex items-center gap-2">
-                  <div className="w-10 h-10 shrink-0 p-2 rounded-full group-hover:bg-emerald-950">
-                    <VomitIcon />
-                  </div>
-                  <div>{delay.vomitsAmount}</div>
-                </button>
-              </Form>
-            </footer>
           </div>
         </Link>
+        <footer className="flex mt-auto justify-end text-zinc-500 p-6 pt-0">
+          <Form
+            action={`/atrasos/${delay.id}/vomit`}
+            method="post"
+            className={vomitActionClasses}
+          >
+            <button
+              name="intent"
+              type="submit"
+              value={delay.hasUserVomited ? "remove" : "add"}
+              className="flex items-center gap-2"
+            >
+              <div className="w-10 h-10 shrink-0 p-2 rounded-full group-hover:bg-emerald-950">
+                <VomitIcon />
+              </div>
+              <div>{delay.vomitsAmount}</div>
+            </button>
+          </Form>
+        </footer>
       </article>
     </li>
   );
